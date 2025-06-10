9588c8497760c4f8e69aca47fb1a0c385773b971
Weiming Yang <iyangwm@hotmail.com>
2025-06-10 22:26:16 +0800

    feat: init front end

---
+++ sangeng-framework/src/test/java/TestHttpStatus.java
@@ -0,0 +1,15 @@
+import org.springframework.http.HttpStatus;
+
+/**
+ * TestHttpStatus
+ *
+ * @author Weiming Yang
+ * @date 2025/2/23
+ */
+
+public class TestHttpStatus {
+    public static void main(String[] args) {
+
+    }
+
+}
